737b20f72f1c28ae7cd7a0e331a3de8e71d2f16c
jalil1994 <hakopedro@gmail.com>
2017-03-05 10:09:21 +0100

    first put of the code in public after a long hour of work

---
+++ Code/Panier.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+
+	<!-- menu responsive :  -->
+        <link rel="stylesheet" href="slicknav/slicknav.css">
+        <script src="js/jquery.js"></script>
+        <script src="slicknav/jquery.slicknav.js"></script>
+
+
+	<link rel="shortcut icon" type="image/x-icon" href="img/logo3.png" />
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link href="bootstrap/css/bootstrap.css" rel="stylesheet">
+    <link href="stylelearn.css" rel="stylesheet">
+    <link href="responsive.css" rel="stylesheet">
+    <meta http-equiv="content-type" content="text/html; charset=utf-8" />	
+	<title>Panier</title>
+						
+</head>
+<body>
+	<div id="page">
+
+<?php include('header.php'); ?>
+
+		<div id="pageQualite">
+			<div id="content">
+				<div class="container">
+					<div class="row">
+<?php include('actu.php'); ?>	
+
+						<div class="col-md-9">
+							<p>Panier / redirection vers connexion si pas connecter</p>
+						</div>
+
+
+
+					</div>
+				</div>
+			</div>	
+		</div>
+		
+		<footer>
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12">
+						<div class="footer">
+							<p>© Smartphonia</p>
+						</div>
+					</div>
+				</div>
+			</div>	
+		</footer>
+		
+	</div>
+</body>
+</html>
