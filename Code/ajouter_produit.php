e098f17be98a4e98a2f516fb3309ac7323c7930d
mebarki01011993abdelghani <ghanimeba@yahoo.fr>
2017-03-08 17:10:51 +0000

    ajout d'un produit (partie admin)

--- Code/ajouter_produit.php
+++ Code/ajouter_produit.php
@@ -1,55 +1,56 @@
-		<header>	
-			<div id="head">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-12">
-							<a href="/Smartphonia/">
-								<p>Marsphonia</p>
-							</a>
 
-						<div id='navigation'>
-							<div  class="nav">
-								<a href="/Smartphonia/">Accueil </a>								
-								<a href="Smartphones.php">Smartphones</a>
-								<a href="Panier.php">Panier</a>
-								<a href="Connexion.php">Connexion</a>
-								<a href="Inscription.php">Inscription</a>
-								<a href="FAQ.php">FAQ</a>
-							</div>
-						</div>	
-						</div>
-					</div>
-				</div>
-			</div>
-			</header>
+    <header>  
+      <div id="head">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <a href="/Smartphonia/">
+                <p>Marsphonia</p>
+              </a>
+
+            <div id='navigation'>
+              <div  class="nav">
+                <a href="/Smartphonia/">Accueil </a>                
+                <a href="Smartphones.php">Smartphones</a>
+                <a href="Panier.php">Panier</a>
+                <a href="Connexion.php">Connexion</a>
+                <a href="Inscription.php">Inscription</a>
+                <a href="FAQ.php">FAQ</a>
+              </div>
+            </div>  
+            </div>
+          </div>
+        </div>
+      </div>
+      </header>
 
 
 
-			<aside>
+      <aside>
 
-				 <li class="sub-menu">
+         <li class="sub-menu">
                      <a href="javascript:;" class="">
                          
                           <span>Gérer les produits </span>
                           <span class="menu-arrow arrow_carrot-right"></span>
                      </a>
-					 <ul class="sub">
+           <ul class="sub">
                           <li><a class="" href="ajouter_produit.php">Ajouter</a></li>
                           <li><a class="" href="modifier_produit.php">Modifier</a></li>
                           <li><a class="" href="supprimer_produit.php">Supprimer </a></li>
                           <li><a class="" href="consulter_produit.php">la liste des produits</a></li>
-                 	</ul>
+                  </ul>
                  </li>
-      		</aside>
+          </aside>
 
 
-      		<!--main content start-->
+          <!--main content start-->
       <section >
           
                            
                             
      
-     <form method="post" action="ajouter_produit.php">
+     <form method="post" action="ajouterunproduit.php">
       <table  border="0" align="center" cellspacing="10" cellpadding="10">
         <tr>
           <td>  <img src="" alt="" class="admin"/> </td>
@@ -78,15 +79,17 @@
         
         <tr>
           <td>   <label for="type" class="pr"  > Marque </label></td>
+
           <td> <input id="adr" name="dim" type="text"  required="" /> 
         </td>
-		
-		<tr>
+    
+    <tr>
           <td>   <label for="type" class="pr"  > Couleur </label></td>
           <td> <input id="adr" name="dim" type="text"  required="" /> 
         </td>
 
         <tr>
+
           <td>   <label for="type" class="pr"  > Ecran </label></td>
           <td> <input id="adr" name="dim" type="text"  required="" /> 
         </td>
@@ -101,13 +104,19 @@
           <td> <input id="adr" name="dim" type="text"  required="" /> 
         </td>
 
+
+         <tr>
+            <td>  <input id="buts" value="Réinitialiser" type="reset"/> </td>
+            <td> <input id="butV" value="Valider" type="submit"  onclick="return(confirm('Etes vous sûr de vouloir ajouter un produit'));"/>    </td>
+         </tr> 
+
   </select>
 
 
-	<script>
-  			$(function(){
-  			    $('.nav').slicknav({
-  			        prependTo: '#navigation'
-  			    });
-  			});
-	</script>
+  <script>
+        $(function(){
+            $('.nav').slicknav({
+                prependTo: '#navigation'
+            });
+        });
+  </script>
