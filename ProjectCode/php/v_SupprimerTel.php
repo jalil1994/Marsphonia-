b700512573d980e9f64ee44a3d74c4a0cb9dd3f2
mebarki01011993abdelghani <ghanimeba@yahoo.fr>
2017-03-23 21:39:21 +0000

    For deleting a phone

---
+++ ProjectCode/php/v_SupprimerTel.php
@@ -0,0 +1,18 @@
+
+		<div id="pageajoutTel">
+
+			<div id="content">
+				<div class="container">
+					<div class="row">
+						<form method="post" action="index.php?nav=SupprimerTel">
+                                                    <!-- choix du tel + btn suppr tel !-->
+                                                    <div class="col-md-3">
+							<br><br>
+									<input type="submit" name="SupprimerTel" id="SupprimerTel" value="Supprimer">
+							</div>
+						</form>					 	
+					</div>
+				</div>
+			</div>	
+
+		</div>
