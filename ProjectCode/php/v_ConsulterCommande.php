0b1d9b52a9a9a2339288cf2c04ef6969c4d01e2e
mebarki01011993abdelghani <ghanimeba@yahoo.fr>
2017-03-23 21:32:09 +0000

    For the order form

---
+++ ProjectCode/php/v_ConsulterCommande.php
@@ -0,0 +1,12 @@
+
+		<div id="pageajoutTel">
+
+			<div id="content">
+				<div class="container">
+					<div class="row">
+                                             <!-- choix du client + choix commande = fiche commande !-->
+					</div>
+				</div>
+			</div>	
+
+		</div>
