048f0138269b11bd3c42005c0695ea6691ea1a76
mebarki01011993abdelghani <ghanimeba@yahoo.fr>
2017-03-23 20:28:02 +0000

    start session

---
+++ ProjectCode/Controleur.php
@@ -0,0 +1,7 @@
+<?php session_start(); 
+require_once ("include/bdd.inc.php");
+require_once ("include/fct.inc.php");
+$pdo = PdoSmartphonia::getpdoSmartphonia();
+$estConnecte = estConnecte();
+	
+?>
