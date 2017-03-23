c2edef1fd67c26d4b553613ea0e2101bff5cc7f2
mebarki01011993abdelghani <ghanimeba@yahoo.fr>
2017-03-23 21:24:06 +0000

    For phone description

---
+++ ProjectCode/php/v_FicheTel.php
@@ -0,0 +1,21 @@
+
+	<div id="pageSmartphones">
+			<div id="content">
+				<div class="container">
+					<div class="row">
+                                            <div class="col-md-4">
+                                                <?php include('V_Side.php');?>
+                                            </div>
+                                            <div class="col-md-8">
+                                                <div class="col-md-6">
+                                                    <p>Photo tel</p>                                                
+                                                </div>
+                                                <div class="col-md-6">
+                                                    <p>Description tel</p>
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+			</div>	
+		</div>
+	</div>
